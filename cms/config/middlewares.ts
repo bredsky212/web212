5aa158699d2e1091178767ae512105558814aa1d
Bredsky212 <bredsky212@proton.me>
2026-01-31 17:27:25 +0100

    feat: scaffold Strapi v5 CMS with compose and schemas

---
+++ cms/config/middlewares.ts
@@ -0,0 +1,20 @@
+export default [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      credentials: true,
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];

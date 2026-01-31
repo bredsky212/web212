0881e4270a04ec3ccca6ece1224e48b91aaa5254
Bredsky212 <bredsky212@proton.me>
2026-01-31 21:24:50 +0100

    chore(cms): tighten env config types

--- cms/config/_types.ts
+++ cms/config/_types.ts
@@ -1,5 +1,5 @@
 ﻿export type EnvFn = {
-  (key: string, defaultValue?: any): any;
+  (key: string, defaultValue?: unknown): unknown;
   bool: (key: string, defaultValue?: boolean) => boolean;
   int: (key: string, defaultValue?: number) => number;
   array: (key: string, defaultValue?: string[]) => string[];
